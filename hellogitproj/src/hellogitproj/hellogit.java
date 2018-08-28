4a28757b5b7d6679765fb48ebc9ec71536c90333
munnaf <munaf.abdul9071@gmail.com>
2018-08-28 11:25:33 +0530

    dev1 2nd cimmit

--- hellogitproj/src/hellogitproj/hellogit.java
+++ hellogitproj/src/hellogitproj/hellogit.java
@@ -5,7 +5,8 @@ public class hellogit {
 	public static void main(String[] args) {
 	    //deve1
 		System.out.println("this is first msg dev1");
-       
+		//dev1
+		System.out.println("this is 2nd msg dev1");
 	}
 
 }
