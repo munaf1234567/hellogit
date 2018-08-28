0190fc8d06fe2785089d78b5a61a19babd661bf8
munnaf <munaf.abdul9071@gmail.com>
2018-08-28 11:35:58 +0530

    dev2 commit at fist msg of dev2

--- hellogitproj/src/hellogitproj/hellogit.java
+++ hellogitproj/src/hellogitproj/hellogit.java
@@ -4,7 +4,7 @@ public class hellogit {
 
 	public static void main(String[] args) {
 	    //deve1
-		System.out.println("this is first msg dev1");
+		System.out.println("this  my fev is first msg dev1");
 		//deve2
 		System.out.println("this is my first msg dev2");
        
