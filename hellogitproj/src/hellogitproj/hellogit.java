a51dc151575a7970872dcad0551fa92efa77b64b
munnaf <munaf.abdul9071@gmail.com>
2018-08-28 11:12:44 +0530

    dev2 first commit

--- hellogitproj/src/hellogitproj/hellogit.java
+++ hellogitproj/src/hellogitproj/hellogit.java
@@ -5,6 +5,8 @@ public class hellogit {
 	public static void main(String[] args) {
 	    //deve1
 		System.out.println("this is first msg dev1");
+		//deve2
+		System.out.println("this is first msg dev2");
        
 	}
 
