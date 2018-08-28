d73cbb9a2c017c903844f48d77b6a4ee227c90fb
munnaf <munaf.abdul9071@gmail.com>
2018-08-28 11:41:44 +0530

    dev 2 3rd

--- hellogitproj/src/hellogitproj/hellogit.java
+++ hellogitproj/src/hellogitproj/hellogit.java
@@ -7,7 +7,7 @@ public class hellogit {
 		System.out.println("this  myee fev is first msg dev1");
 		//deve2
 		System.out.println("this is my first msg dev2");
-       
+		System.out.println("this is my first msg dev2");
 	}
 
 }
